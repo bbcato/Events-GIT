04d47ea85f3678a51ae38731c84550b1e7ef6250
bairon bohorquez <dhbairon@gmail.com>
2013-11-21 22:32:27 -0500

    Ultima actualizacion
    Queda listo el pantallazo del formulario de reseservas , solo queda
    pendiente arregalr el boton

--- invitacion/form/asistencia.php
+++ invitacion/form/asistencia.php
@@ -19,23 +19,11 @@
 
 	<!-- CSS
   ================================================== -->
-  <link rel="stylesheet" href="../css/estilos.css" />
+  <link rel="stylesheet" href="../css/stilos.css" />
   <link rel="stylesheet" href="../css/normalize.css" />
-  <link rel="stylesheet" href="../css/responsive.css" />
-  <link rel="stylesheet" href="../css/responsive-ipad-horizontal.css" />
-
-  <link rel="stylesheet" type="text/css" href="../font/roboto_thin/stylesheet.css">
-  <link rel="stylesheet" type="text/css" href="../font/roboto_bold/stylesheet.css">
-
-
   
+  <link rel="stylesheet" type="text/css" href="../css/font/Raleway/stylesheet.css">
 
-
-	
-
-	
-
-	
 
 </head>
 
@@ -63,32 +51,54 @@ if (!isset($_POST['Mail'])) {
   mail($destino,$asunto,$mensaje,"FROM: $remitente");
 ?>
 
-  <section id="menu_principal_formcoti">
-    <div id="cont_menu_principal_formcoti">
-      <img src="../img/logo-w.png">
-      <div id="cont_rs_mp_formcoti">
-        <a href="https://www.facebook.com/eventsdh" target="blank">F</a>
-        <a href="https://twitter.com/dhcolevent" target="blank">T</a>
+  <nav>
+    <div id="cont_nav">
+      <div class="box_btn_menu">
+        <a href="../index.html" class="btn_menu_inv"><img src="../images/perfil.png"></a>
       </div>
+      <div class="box_btn_menu">
+        <a href="../ubicacion.html" class="btn_menu_inv"><img src="../images/locacion.png"></a>
+      </div>
+      <div class="box_btn_menu">
+        <a href="../mensaje.html" class="btn_menu_inv"><img src="../images/mensaje.png"></a>
+      </div>
+      <div class="box_btn_menu color_bc">
+        <a href="../organizador.html" class="btn_menu_inv"><img src="../images/favorito.png"></a>
+      </div>
+      
     </div>
+  </nav>
+
+
+  <section id="Datos_inv">
+    <div id="Cont_Datos_inv">
+
+      <p id="Name_principal">Tu Confirmacion a sido recibida.</p>
+      <p id="Type_evento">Este evento es coordinado y organizado por:</p>
+      <div id="cont_fecha_inv">
+        <figure><img src="../images/icon_calendar.png" class="object_fecha_inv"> <p id="Fecha_evento" class="object_fecha_inv">Arbey eventos , tu mejor elección</p></figure>
+        
+      </div>
+
+    </div>
+
   </section>
 
  
-  <section id="box_msn_send_form_coti">
-    <div id="cont_box_msn_send_form_coti">
+  <section id="Descripcion_msn_send">
+    <div id="Cont_Description_msn_send">
 
-      <p id="title_recibido_form">Mensaje recibido - gracias por contactarnos</p>
-      <img src="https://fbcdn-sphotos-f-a.akamaihd.net/hphotos-ak-prn1/1394_359188700892952_1876555254_n.jpg">
-      <p>Arbey eventos , tu mejor elección</p>
+      <img src="https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-ash4/1003899_377147855763703_928844088_n.png">
+      <p>Vas aorganizar una fiesta pronto?</p>
+      <p> Cotizar tu evento con nosotros.</p>
+      <button><a href="#">Aqui</a></button>
 
   </section>
 
-  <section id="footer">
-    <div id="cont_footer">
-      <img src="../img/logo.png">
+  <footer>
       <p>© 2013 DH-GC S.A.S All Rights Reserved.</p>
-    </div>
-  </section>
+  </footer>
+    
 
 <?php
 }
